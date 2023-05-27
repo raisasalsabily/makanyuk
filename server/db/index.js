5e85bfda1458ae6919b45200d3a5c4eb900a2c71
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-05-27 07:11:43 +0700

    fix db config

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 mongoose
-    .connect('mongodb://localhost:27017/food-ordering', { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+  .connect(
+    "mongodb+srv://makanyuk-admin:yYke1KzP2RJDmWxY@makanyuk-cluster.qpsz3gi.mongodb.net/makanyukdb?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .catch((e) => {
+    console.error("Connection error", e.message)
+  })
 
 const db = mongoose.connection
 

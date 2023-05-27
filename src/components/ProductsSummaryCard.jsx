0fb21a4391fb87bb39a0c4a549b738293a5ec5b6
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-05-27 09:42:18 +0700

    fix cart ui

--- src/components/ProductsSummaryCard.jsx
+++ src/components/ProductsSummaryCard.jsx
@@ -11,32 +11,40 @@ export const ProductsSummaryCard = ({ product }) => {
 
   return (
     <div className="flex p-1 sm:p-2 border-b border-b-gray-200">
-      <div className="product-image mr-2 border border-grey-200 rounded-lg w-full sm:w-1/3">
+      <div className="product-image mr-2 border border-grey-200 rounded-lg">
         <img src={PastaTemp} alt={"pastatemp"} className="w-24 h-24" />
       </div>
-      <div className="product-info">
-        <h3 className="font-semibold">{product.name}</h3>
-        <p className="text-gray-600">{product.description}</p>
-      </div>
-      <div className="product-price-qt flex flex-col items-center justify-center">
-        <div className="price">{`Rp ${product.price}`}</div>
-        <div className="quantity flex">
-          <button
-            className="p-1"
-            disabled={product.amount <= 0}
-            onClick={() => dispatch(decrementProductAmount(product))}
-          >
-            -
-          </button>
-          <span className="p-1">{product.amount}</span>
-          <button
-            className="p-1"
-            onClick={() => dispatch(incrementProductAmount(product))}
-          >
-            +
-          </button>
+      {/* start - card main  */}
+      <div id="card__main" className="flex flex-col gap-3">
+        {/* nama dan deskripsi produk */}
+        <div className="product-info">
+          <h3 className="font-semibold">{product.name}</h3>
+          <p className="text-gray-600">{product.description}</p>
         </div>
+        {/* end- nama dan deskripsi produk */}
+        {/* harga dan kuantitas produk */}
+        <div className="product-price-qt flex flex-col items-center justify-center">
+          <div className="price">{`Rp ${product.price}`}</div>
+          <div className="quantity flex justify-center items-center">
+            <button
+              className="font-bold p-1 h-5 w-5 rounded-md bg-yellow-500 text-white flex justify-center items-center"
+              disabled={product.amount <= 0}
+              onClick={() => dispatch(decrementProductAmount(product))}
+            >
+              -
+            </button>
+            <span className="p-1">{product.amount}</span>
+            <button
+              className="font-bold p-1 h-5 w-5 rounded-md bg-yellow-500 text-white flex justify-center items-center"
+              onClick={() => dispatch(incrementProductAmount(product))}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        {/* end - harga dan kuantitas produk */}
       </div>
+      {/* end - card main  */}
     </div>
   )
 }

c4cc6ee09e7ca0c82f920e2c40f9f3d45c240add
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-05-30 17:10:19 +0700

    Update ProductPreviewCard.jsx

--- src/components/ProductPreviewCard.jsx
+++ src/components/ProductPreviewCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AddProduct } from "./AddProduct";
-import PastaTemp from "../../src/assets/images/pastatemp.jpg"
+import PastaTemp from "../../src/assets/images/Spaghetti Aglio e Olio.jpg"
 
 export const ProductPreviewCard = ({ product, onAddProduct }) => {
   const addProduct = () => {
@@ -9,7 +9,10 @@ export const ProductPreviewCard = ({ product, onAddProduct }) => {
 
   return (
     <div className="w-full p-4 m-2 rounded text-black bg-white text-center border drop-shadow-lg">
-      <img src={PastaTemp} alt={product.name} />
+      <div className="flex flex-col
+       items-center">
+        <img className="w-3/6 flex" src={PastaTemp} alt={product.name} /> 
+      </div>
       <h2 className="pb-2 text-b-md font-bold font-poppins">{product.name}</h2>
       <p className="mb-2 h-20 line-clamp-4">{product.description}</p>
       <AddProduct onAddProduct={addProduct} />

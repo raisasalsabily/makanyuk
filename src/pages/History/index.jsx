b3cfd5a687090b9b2800ed977e23e4e45d2b77ff
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-06-04 17:06:21 +0700

    fix: fix order history style

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -11,51 +11,51 @@ import axios from "axios"
 //   },
 // ]
 
-const historyData = [
-  {
-    shippingAddress: {
-      custName: "Raisa Adriana",
-      custPhone: "081187238",
-      address: "jalan kebenrana9999",
-      city: "solo",
-      postalCode: "1293",
-      country: "indones",
-    },
-    _id: "647c345de4bf223048bd53fc",
-    orderItems: [
-      {
-        name: "Aglio Olio",
-        amount: 1,
-        price: 50000,
-        product: "pasta",
-        _id: "647c345de4bf223048bd53fd",
-      },
-      {
-        name: "Bolognese",
-        amount: 1,
-        price: 58000,
-        product: "pasta",
-        _id: "647c345de4bf223048bd53fe",
-      },
-      {
-        name: "Carbonara",
-        amount: 1,
-        price: 66000,
-        product: "pasta",
-        _id: "647c345de4bf223048bd53ff",
-      },
-    ],
-    paymentMethod: "COD",
-    taxPrice: 0,
-    shippingPrice: 0,
-    totalPrice: 174000,
-    isPaid: false,
-    isDelivered: false,
-    createdAt: "2023-06-04T06:51:09.927Z",
-    updatedAt: "2023-06-04T06:51:09.927Z",
-    __v: 0,
-  },
-]
+// const historyData = [
+//   {
+//     shippingAddress: {
+//       custName: "Raisa Adriana",
+//       custPhone: "081187238",
+//       address: "jalan kebenrana9999",
+//       city: "solo",
+//       postalCode: "1293",
+//       country: "indones",
+//     },
+//     _id: "647c345de4bf223048bd53fc",
+//     orderItems: [
+//       {
+//         name: "Aglio Olio",
+//         amount: 1,
+//         price: 50000,
+//         product: "pasta",
+//         _id: "647c345de4bf223048bd53fd",
+//       },
+//       {
+//         name: "Bolognese",
+//         amount: 1,
+//         price: 58000,
+//         product: "pasta",
+//         _id: "647c345de4bf223048bd53fe",
+//       },
+//       {
+//         name: "Carbonara",
+//         amount: 1,
+//         price: 66000,
+//         product: "pasta",
+//         _id: "647c345de4bf223048bd53ff",
+//       },
+//     ],
+//     paymentMethod: "COD",
+//     taxPrice: 0,
+//     shippingPrice: 0,
+//     totalPrice: 174000,
+//     isPaid: false,
+//     isDelivered: false,
+//     createdAt: "2023-06-04T06:51:09.927Z",
+//     updatedAt: "2023-06-04T06:51:09.927Z",
+//     __v: 0,
+//   },
+// ]
 
 const History = () => {
   const [orders, setOrders] = useState([])
@@ -85,51 +85,67 @@ const History = () => {
 
   return (
     <div>
-      {/* title */}
-      <div>Histori Pemesanan</div>
-      {/* main */}
-      <main>
-        {/* order item */}
-        {orders?.data ? (
-          orders?.data?.map((item) => (
-            <div>
-              {/* order item start */}
-              <div
-                id="order__item"
-                className="border-2 border-neutral-500 rounded-md max-w-[600px]"
-              >
-                <table class="table-fixed">
-                  <tbody>
-                    <tr>
-                      <td className="px-4 py-2">Nama penerima</td>
-                      <td className="px-4 py-2">
-                        {item.shippingAddress.custName}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 py-2">Alamat</td>
-                      <td className="px-4 py-2">
-                        {item.shippingAddress.address}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 py-2">Kota</td>
-                      <td className="px-4 py-2">{item.shippingAddress.city}</td>
-                    </tr>
-                    <tr>
-                      <td className="px-4 py-2">Total harga</td>
-                      <td className="px-4 py-2">{item.totalPrice}</td>
-                    </tr>
-                  </tbody>
-                </table>
+      <div id="container" className="flex flex-col">
+        {/* title */}
+        <div className="text-h-md font-semibold bg-amber-300 flex justify-center items-center my-8 py-4">
+          Histori Pemesanan
+        </div>
+        {/* main */}
+        <main className="mx-4 lg:mx-16">
+          {/* order item */}
+          {orders?.data ? (
+            orders?.data?.map((item) => (
+              <div>
+                {/* order item start */}
+                <div
+                  id="order__item"
+                  className="my-4 border-2 border-neutral-500 rounded-md w-[98%] max-w-[800px]"
+                >
+                  <table class="table-fixed">
+                    <tbody>
+                      <tr>
+                        <td className="px-4 py-2">Nama penerima</td>
+                        <td className="px-4 py-2">
+                          {item.shippingAddress.custName}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="px-4 py-2">Alamat</td>
+                        <td className="px-4 py-2">
+                          {item.shippingAddress.address},{" "}
+                          {item.shippingAddress.city},{" "}
+                          {item.shippingAddress.postalCode}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="px-4 py-2">Item</td>
+                        <td className="px-4 py-2">
+                          {" "}
+                          {item.orderItems.map((orderItem) => (
+                            <div>
+                              <p>
+                                {orderItem.name} x {orderItem.amount} :{" "}
+                                {orderItem.price}
+                              </p>
+                            </div>
+                          ))}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="px-4 py-2">Total harga</td>
+                        <td className="px-4 py-2">{item.totalPrice}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                {/* end - order item */}
               </div>
-              {/* end - order item */}
-            </div>
-          ))
-        ) : (
-          <div>belum ada pemesanan</div>
-        )}
-      </main>
+            ))
+          ) : (
+            <div>belum ada pemesanan</div>
+          )}
+        </main>
+      </div>
     </div>
   )
 }
